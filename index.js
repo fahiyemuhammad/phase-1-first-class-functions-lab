22aad8c1943c7b5dfc55be556c7c9bd4cb8fc30f
Fahiye Muhammad <fahiye254@gmail.com>
2025-03-07 22:49:32 +0300

    Generating first class functions

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+let driversNames =['Antonia', 'Nuru', 'Amari', 'Mo']
+ const returnFirstTwoDrivers = function (driversNames){
+
+    return driversNames.slice(0,2)
+ }
+ 
+
+ const returnLastTwoDrivers = function(driversNames){
+    return driversNames.slice(2)
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+ const createFareMultiplier = function  (num){
+   return function (fare){
+    return fare * num
+   }
+ }
+
+ function fareDoubler(fare){
+return fare * 2
+ }
+
+ function fareTripler(fare){
+    return fare * 3
+ }
+
+ 
+  
+  const selectDifferentDrivers = function(arrayOfDrivers, driverSelectorFunction) {
+    return driverSelectorFunction(arrayOfDrivers);
+  };
